93d357244af5f0cebdecadade07d13a0b3e8ac9b
FrancescoSereno <francescousereno@gmail.com>
2026-04-01 12:38:50 +0200

    Waypoint generator + Voronoi updated
    Implementazione di k-means per generare i waypoint.
    
    Aggiornamento del Voronoi partitioning per dividere lo spazio in base ai waypoint e non al free space.
    Aggiornamento di plot_voronoi

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import sys
 from pathlib import Path
+import math
 
 # Set the root directory and add the source directory to the Python path
 ROOT = Path(__file__).resolve().parent
@@ -39,6 +40,28 @@ def build_demo(config):
     for i, pos in enumerate(drone_positions):
         print(f"  Drone {i+1}: {pos}")
 
+    #Sto assumendo camera con un FOV diagonale di 84° (abbastanza largo) a un'altezza h dal terreno
+    H_FOV = 71.1 #Horizontal FOV is equal to 71.1°
+    V_FOV = 56.3 #Vertical FOV is equal to 56.3°
+    h = 5 #Distanza del drone dal livello 0 del terreno
+
+    L = 2* h * math.tan(math.radians(H_FOV/2)) # Largehzza immagine
+    W = 2* h * math.tan(math.radians(V_FOV/2)) # Altezza immagine
+
+    A_FOV = L * W
+    A_map = map3d.x_bounds[1] * map3d.y_bounds[1]
+    k = math.ceil(1.2 * A_map/A_FOV)
+
+    print(f"Output image dimensions: {L} meters of width and {W} meters of height.")
+    print(f"Map area = {A_map} m^2")
+    print(f"FOV area = {A_FOV} m^2")
+    print(f"k = {k}")
+
+    waypoints = kmeans_clustering(
+            map3d.free_space,
+            k,
+            seed=seed,
+        )
 
     # --- Voronoi Partition --- 
     print("Computing Voronoi partition for the generated map and drone starting positions...")
@@ -46,6 +69,7 @@ def build_demo(config):
         map3d.free_space,
         uav_cfg["num_uavs"],
         seed=seed,
+        waypoints=waypoints
     )
 
     print("Voronoi seeds from k-means on free space:")
@@ -58,7 +82,7 @@ def build_demo(config):
         map3D=map3d,
     )
 
-    return map3d, vor, drone_positions
+    return map3d, vor, drone_positions, waypoints
 
 
 if __name__ == "__main__":
@@ -70,13 +94,14 @@ if __name__ == "__main__":
     seed_everything(config["seed"])
 
     # Build the demo environment and get initial drone positions
-    map3d, vor, drone_positions = build_demo(config)
+    map3d, vor, drone_positions, waypoints = build_demo(config)
     
     # Visualize the Voronoi partition together with obstacles and initial drone positions
     plot_voronoi_partition(
         map3d,
         vor,
         drone_positions=drone_positions,
+        waypoints=waypoints,
         title="Voronoi Partition of the Workspace",
     )
 
