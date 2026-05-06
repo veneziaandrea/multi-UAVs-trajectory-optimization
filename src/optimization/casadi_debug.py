a9db6f0217151f2151bd6790cdc3c7f69f2782a7
francescostreet <strfra02@gmail.com>
2026-05-06 18:46:53 +0200

    removed test files

--- src/optimization/casadi_debug.py
+++
@@ -1,7 +0,0 @@
-import casadi
-try:
-    # This specifically tries to load the OSQP engine
-    solver = casadi.conic('solver', 'osqp', {'x': casadi.MX.sym('x')})
-    print("SUCCESS: OSQP is working!")
-except Exception as e:
-    print(f"STILL FAILING: {e}")
